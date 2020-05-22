7f0b72d7a000a551e581aa415e49a8f40956e327
kevinArgueta96 <kev200050@gmail.com>
2020-05-22 14:38:27 -0600

    Ingreso datos

---
+++ src/Logica/conexcion.java
@@ -0,0 +1,41 @@
+package Logica;
+
+import java.sql.*;
+import java.awt.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author argue
+ */
+public class conexcion {
+
+    static Connection cnc;
+    static Statement st;
+    private static final String user = "db_rem";
+    private static final String pass = "db_rem";
+    private static final String url = "jdbc:oracle:thin:@localhost:1521:orcl";
+
+    public conexcion() {
+        try {
+            cnc = DriverManager.getConnection(url, user, pass);
+            st = cnc.createStatement();
+            if (cnc != null) {
+                //JOptionPane.showMessageDialog(null, "Conexcion exitosa");
+            }
+        } catch (Exception ex) {
+            //JOptionPane.showMessageDialog(null, "Conexcion fallida"+ ex);
+        }
+    }
+
+    public Statement getConnection() {
+        return st;
+    }
+
+    public void desconectar() {
+        try {
+            cnc.close();
+        } catch (Exception ex) {
+        }
+    }
+}
